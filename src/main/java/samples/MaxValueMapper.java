dedc94e360de8deda5a695ca783a5f1b74118fd4
Tharindu Rusira <tharindurusira@gmail.com>
2014-01-23 18:09:26 +0530

    changed IntWritable to LongWritable.

--- src/main/java/samples/MaxValueMapper.java
+++ src/main/java/samples/MaxValueMapper.java
@@ -2,12 +2,11 @@ package samples;
 
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MaxValueMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class MaxValueMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
 	private static final int MISSING = 9999;
 	@Override
@@ -15,15 +14,15 @@ public class MaxValueMapper extends Mapper<LongWritable, Text, Text, IntWritable
 			throws IOException,InterruptedException{
 		String line = value.toString();
 		String year = line.substring(15,19);
-		int airTemperature;
+		long airTemperature;
 		if(line.charAt(87) == '+'){
-			airTemperature = Integer.parseInt(line.substring(88,92));
+			airTemperature = Long.parseLong(line.substring(88,92));
 		}else{
-			airTemperature= Integer.parseInt(line.substring(87, 92));
+			airTemperature= Long.parseLong(line.substring(87, 92));
 		}
 		String quality = line.substring(92, 93);
 		if(airTemperature != MISSING && quality.matches("[01459]")){
-			context.write(new Text(year), new IntWritable(airTemperature));
+			context.write(new Text(year), new LongWritable(airTemperature));
 		}
 	}
 }
--- src/main/java/samples/MaxValueReducer.java
+++ src/main/java/samples/MaxValueReducer.java
@@ -2,20 +2,20 @@ package samples;
 
 import java.io.IOException;
 
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class MaxValueReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class MaxValueReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
 	
 	@Override
-	public void reduce(Text key,Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+	public void reduce(Text key,Iterable<LongWritable> values, Context context) throws IOException, InterruptedException{
 		
-		int maxValue = Integer.MIN_VALUE;
-		for(IntWritable value : values){
+		long maxValue = Integer.MIN_VALUE;
+		for(LongWritable value : values){
 			maxValue = Math.max(maxValue, value.get());
 		}
-		context.write(key, new IntWritable(maxValue));
+		context.write(key, new LongWritable(maxValue));
 		
 	}
 
